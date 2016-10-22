f924dfd55390f4af9369a80ae3de4fd059efc654
vahbuna <silver.estla@gmail.com>
2016-10-22 16:46:22 +0200

    Updated java code

--- java/src/anu/kwic/cmd/KwicApp.java
+++ java/src/anu/kwic/cmd/KwicApp.java
@@ -12,9 +12,9 @@ import java.util.List;
 import java.util.Map;
 
 public class KwicApp {
-  private File fileObj;
+  private final File fileObj;
   private final String word;
-  private int contextLength;
+  private final int contextLength;
   private List<Map.Entry<String, String>> contexts;
 
   public KwicApp(final String filePath, final String word, final int context)
@@ -23,7 +23,6 @@ public class KwicApp {
     this.contextLength = context;
     this.fileObj = new File(filePath);
     if (!this.fileObj.exists()) {
-      this.fileObj = null;
       throw new FileNotFoundException("Given file: " + filePath + " does "
             + "not exist");
     }
@@ -35,45 +34,52 @@ public class KwicApp {
 
   public void searchContext() {
     ArrayList<String> tokenList = new ArrayList<String>();
-    try (BufferedReader br =
+    try (BufferedReader buffReader =
       new BufferedReader(new FileReader(this.fileObj))) {
       String line;
-      while ((line = br.readLine()) != null) {
-        String[] lineTokens = line.split(" ");
+      while ((line = buffReader.readLine()) != null) {
+        String[] lineTokens = null;
+        lineTokens = line.split(" ");
         tokenList.addAll(Arrays.asList(lineTokens));
       }
     } catch (IOException ioexp) {
       System.err.println(ioexp.getMessage());
     }
     if (!tokenList.isEmpty() && tokenList.contains(word)) {
+      StringBuilder rightContext = new StringBuilder();
+      StringBuilder leftContext = new StringBuilder();
+
+      Map<String, String> pair = new HashMap<String, String>();
       this.contexts = new ArrayList<Map.Entry<String, String>>();
-      int lastIdx = tokenList.size();
+      final int lastIdx = tokenList.size();
       for (int i = 0;  i < lastIdx - 1; i++) {
         if (word.equals(tokenList.get(i))) {
-          StringBuilder rightContext = new StringBuilder();
-          StringBuilder leftContext = new StringBuilder();
           for (int j = i + 1; j < lastIdx && j - i <= this.contextLength; j++) {
             rightContext.append(tokenList.get(j));
-            rightContext.append(" ");
+            rightContext.append(' ');
           }
-          
+
           for (int j = i - 1; j > 0 && i - j <= this.contextLength; j--) {
             leftContext.insert(0, tokenList.get(j) + " ");
             //System.out.println(tokenList.get(j));
           }
-          Map<String, String> pair = new HashMap<String, String>();
+
           pair.put(leftContext.toString(), rightContext.toString());
           this.contexts.addAll(pair.entrySet());
+          rightContext.setLength(0);
+          leftContext.setLength(0);
+          pair.clear();
         }
       }
     }
   }
 
   public void displayConcordance() {
-    if (this.contexts != null) {
-      for (int i = 0; i < this.contexts.size(); i++) {
-        Map.Entry<String, String> contextDico = this.contexts.get(i);
-        System.out.format("%s\t%s\t%s\n", contextDico.getKey(),
+    final List<Map.Entry<String, String>> output = getContexts();
+    if (output != null) {
+      for (int i = 0; i < output.size(); i++) {
+        Map.Entry<String, String> contextDico = output.get(i);
+        System.out.format("%s\t%s\t%s%n", contextDico.getKey(),
             this.word, contextDico.getValue());
       }
     }
@@ -89,55 +95,63 @@ public class KwicApp {
   /**
    * @param args arguments file, word and context.
    */
+  
+  static Map<String, String> parseArguments(final String[] args) {
+    Map<String, String> argMap = new HashMap<String, String>();
+    final int argsSize = 6;
 
-  public static void main(String[] args) {
-    String filePath = null;
-    String word = null;
-    int contextSize = -1;
-    KwicApp kwicInfo = null;
-    if (args.length != 6) {
-      displayHelp();
-    } else {
-      Map<String, String> argMap = new HashMap<String, String>();
+    if (args.length == argsSize) {
       for (int i = 0; i < 5; i += 2) {
-        if (args[i].startsWith("-")) {
+        if (args[i].charAt(0) == '-') {
           String key = args[i].substring(1).toLowerCase();
           argMap.put(key, args[i + 1]);
         } else {
           System.out.println("Syntax Error");
           displayHelp();
-          return;
+          argMap.clear();
         }
       }
-
       if (argMap.containsKey("file") && argMap.containsKey("word")
           && argMap.containsKey("context")) {
-        filePath = argMap.get("file");
-        word = argMap.get("word");
-
-        try {
-          contextSize = Integer.parseInt(argMap.get("context"));
-          if (contextSize < 0) {
-            throw new NumberFormatException("negative context");
-          }
-        } catch (NumberFormatException numexp) {
-          System.err.println("NumberFormatException: " + numexp.getMessage());
-          System.out.println("Expecting a number for the context size");
+        int contextSize = Integer.parseInt(argMap.get("context"));
+        if (contextSize < 0) {
+          throw new NumberFormatException("negative context");
         }
-
-        try {
-          kwicInfo = new KwicApp(filePath, word, contextSize);
-          kwicInfo.searchContext();
-          kwicInfo.displayConcordance();
-        } catch (Exception exp) {
-          System.err.println(exp.getMessage());
-        } finally {
-          kwicInfo = null;
-        }
-      } else {
-        System.out.println("Syntax Error");
-        displayHelp();
       }
+    } else {
+      displayHelp();
+    }
+    return argMap;
+  }
+
+  public static void main(final String[] args) {
+
+    Map<String, String> argMap;
+    argMap = parseArguments(args);
+
+    if (argMap.containsKey("file") && argMap.containsKey("word")
+        && argMap.containsKey("context")) {
+      String filePath;
+      filePath = argMap.get("file");
+      String word;
+      word = argMap.get("word");
+      int contextSize = -1;
+
+      try {
+        contextSize = Integer.parseInt(argMap.get("context"));
+        KwicApp kwicInfo;
+        kwicInfo = new KwicApp(filePath, word, contextSize);
+        kwicInfo.searchContext();
+        kwicInfo.displayConcordance();
+      } catch (NumberFormatException numexp) {
+        System.err.println("NumberFormatException: " + numexp.getMessage());
+        System.out.println("Expecting a number for the context size");
+      } catch (Exception exp) {
+        System.err.println(exp.getMessage());
+      }
+    } else {
+      System.out.println("Syntax Error");
+      displayHelp();
     }
   }
 }
